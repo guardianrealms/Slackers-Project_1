7e6eea845f4ba873db11478c05103ded964eb3b4
sonofprime <39097635+sonofprime@users.noreply.github.com>
2018-05-10 10:20:56 -0500

    playerCamera
    camera movent needs work, and the player movent

---
+++ Slackers-project_one/Assets/_scripts/PlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    CharacterController charControl;
+    public float walkSpeed;
+
+     void Awake()
+    {
+        charControl = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MovePlayer();   
+    }
+
+    void MovePlayer()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        Vector3 moveDirSide = transform.right * horiz * walkSpeed;
+        Vector3 moveDirForward = transform.forward * vert * walkSpeed;
+
+        charControl.SimpleMove(moveDirSide);
+        charControl.SimpleMove(moveDirForward);
+
+
+    }
+}
--- Slackers-project_one/Assets/_scripts/player.cs
+++ Slackers-project_one/Assets/_scripts/player.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public class player : MonoBehaviour
+{
+    public Transform playerBody;
+    public float mouseSensitivity;
 
-public class player : MonoBehaviour {
+    float xAxisClamp = 0.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        RotateCamera();
 	}
+
+    void RotateCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        float rotAmountX = mouseX = mouseSensitivity;
+        float rotAmountY = mouseY = mouseSensitivity;
+
+        xAxisClamp -= rotAmountY;
+
+        Vector3 targetRotCam = transform.rotation.eulerAngles;
+        Vector3 targetRotBody = playerBody.rotation.eulerAngles;
+
+        targetRotCam.x -= rotAmountY;
+        targetRotCam.z = 0;
+        targetRotBody.y += rotAmountX;
+
+        if(xAxisClamp > 90)
+        {
+            xAxisClamp = 90;
+          targetRotCam.x = 90;
+        }
+        else if (xAxisClamp < -90)
+        { xAxisClamp = -90;
+            targetRotCam.x = 270;
+        }
+
+        transform.rotation = Quaternion.Euler(targetRotCam);
+        playerBody.rotation = Quaternion.Euler(targetRotBody);
+
+    }
+
 }
